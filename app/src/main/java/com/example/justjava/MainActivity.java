ddbc8228b1617e086a5aa9efc27314c6c2f3a22a
MwasCipher <mwangicollinswaweru@gmail.com>
2019-11-15 16:45:19 +0300

    User Input

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,12 +2,63 @@ package com.example.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count, price;
+    TextView countView, priceSummaryTV;
+    Button summaryButton,addCountBtn, reduceCountBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        count = 0;
+        countView = findViewById(R.id.countTextView);
+        priceSummaryTV = findViewById(R.id.priceSummary);
+        summaryButton = findViewById(R.id.summariseButton);
+        addCountBtn = findViewById(R.id.addCountButton);
+        reduceCountBtn = findViewById(R.id.deductCountButton);
+
+        countView.setText(count);
+
+        reduceCountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(count == 0){
+                    reduceCountBtn.setActivated(false);
+                    count = 0;
+                    countView.setText(count);
+                }else {
+
+                    count +=1;
+                    countView.setText(count);
+                }
+            }
+        });
+
+        addCountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               count -=1 ;
+               countView.setText(count);
+
+            }
+        });
+
+        summaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                price = 3* count;
+                priceSummaryTV.setText("Total Cost: " + "$" + price  + "\n Thank You Come Again!!!" );
+            }
+        });
+
     }
 }
